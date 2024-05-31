efebb3f9bc211d6cac28eabcfcc22a4c77cfb9dc
esdrascosta <esdras.cts@outlook.com>
2024-05-31 16:36:04 -0300

    refactor: Adding parameters to Banner module

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -5,7 +7,10 @@ function App() {
   return (
     <>
       <Header />
-      <h1>Hello word, by @deCosta</h1>
+      <Banner image="home" />
+      <Container>
+        <h1>Hello word, by @deCosta</h1>
+      </Container>
       <Footer />
     </>
   );
