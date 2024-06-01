e5d82931827c1c51f18880346dda8b16853f18e3
esdrascosta <esdras.cts@outlook.com>
2024-06-01 09:37:02 -0300

    refactor: Refactoring to take the videos content dynamically

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
+import Category from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import videos from './json/videos.json'
+
+const categories = 
+[
+  "Professional", "Kids", "Planet", "Football", "Nature"
+]
+
+function filterCategories(id)
+{
+  return videos.filter( video => video.category === categories[id] )
+}
 
 function App() {
   return (
@@ -11,10 +22,20 @@ function App() {
       <Header />
       <Banner image="home" />
       <Container>
-        <h1>Hello word, by @deCosta</h1>
+
+        {/* <h1>Hello word, by @deCosta</h1>
         <section className="cards">
           { videos.map(video => <Card id={video.id} key={video.id} /> )}
-        </section>
+        </section> */}
+
+        <Category category={categories[0]}>
+          { filterCategories(0).map((video) => <Card id={video.id} key={video.id} /> )}
+        </Category>
+
+        <Category category={categories[1]}>
+          { filterCategories(1).map((video) => <Card id={video.id} key={video.id} /> )}
+        </Category>
+
       </Container>
       <Footer />
     </>
