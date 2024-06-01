647c8a53ea0a131547c967d7ef1e1969928c4b8a
esdrascosta <esdras.cts@outlook.com>
2024-06-01 08:35:08 -0300

    feat: Adding new component Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,14 @@
 import styles from './Card.module.css'
 
-function Card()
+function Card({ id })
 {
     return (
         <section className={styles.card}>
             <a
-                href="https://www.youtube.com/watch?v=qJ2oiv1c7U8"
+                href={`https://www.youtube.com/watch?v=${id}`}
                 rel='noreferrer noopener'
                 target='_blank'>
-                    <img src="https://img.youtube.com/vi/qJ2oiv1c7U8/mqdefault.jpg" alt='Capa' />
+                    <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} alt='Capa' />
             </a>
         </section>
     );
