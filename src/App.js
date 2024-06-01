d350258b38019787d184de1dca6293fe6ce18a2f
esdrascosta <esdras.cts@outlook.com>
2024-06-01 08:37:13 -0300

    refactor: Refactoring to add new component and display card content dinamically

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Card from "./components/Card";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import videos from './json/videos.json'
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <Banner image="home" />
       <Container>
         <h1>Hello word, by @deCosta</h1>
-        <Card />
+        <section className="cards">
+          { videos.map(video => <Card id={video.id} key={video.id} /> )}
+        </section>
       </Container>
       <Footer />
     </>
